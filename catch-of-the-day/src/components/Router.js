cd0f0044c70926663a944eb77c80d6131ef076c0
iqdanika <iqdanika@gmail.com>
2021-12-27 14:53:57 +0100

    'master'

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -8,7 +8,8 @@ const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={StorePicker} />
-
+      <Route path="/store/:storeId" component={App} />      
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
