1d8835d4305c329483bd2e9945650e9231845d44
iqdanika <iqdanika@gmail.com>
2021-12-30 01:35:00 +0100

    'master'

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -7,6 +7,18 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. Take a copy of the exsitting state
+    const fishes = {...this.state.fishes};
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({ fishes });
+  };
   render() {
     return (
     <div className='catch-of-the-day'>
@@ -14,7 +26,7 @@ class App extends React.Component {
         <Header  tagline="Fresh Seafood Market"/>
       </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
     </div>
     )
   }
